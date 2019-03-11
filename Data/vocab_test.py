52dccf6cc03981ab0e0fde05bd42a2fd92c440a7
Arran Lyon <arran.lyon@hotmail.com>
2019-03-12 04:34:35 +0530

    vocab test

---
+++ Data/vocab_test.py
@@ -0,0 +1,27 @@
+import pickle as pkl
+
+data =[]
+for i in range(1, 33):
+    with open('music21/music_data_{:04d}.pkl'.format(i), 'rb') as f:
+        data.extend(pkl.load(f))
+
+print('Data loaded')
+print(len(data))
+
+beat_vocab ={}
+
+for song in data:
+    ins = song['instruments']
+    for i in range(ins):
+        rhythm = song[i]['rhythm']
+        for bar in rhythm:
+            for beat in bar:
+                if beat in beat_vocab:
+                    beat_vocab[beat] += 1
+                else:
+                    beat_vocab[beat] = 1
+
+for k, v in beat_vocab.items():
+    print(k, v)
+
+print(len(beat_vocab))
