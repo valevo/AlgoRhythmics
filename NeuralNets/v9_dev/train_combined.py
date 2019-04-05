18ffcce00d311e0a118b17889f6dca4a6f94688f
valevo <valevogelmann@gmail.com>
2019-04-05 02:29:48 +0200

    first training with lead

--- NeuralNets/v9_dev/train_combined.py
+++ NeuralNets/v9_dev/train_combined.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#%%
+
 from Data.DataGeneratorsLeadMeta import CombinedGenerator
 
 from Nets.MetaEmbedding import MetaEmbedding
@@ -18,27 +18,15 @@ import os
 
 from tensorflow.python.keras.callbacks import TensorBoard
 
-def without_lead(cg_inst, rc, mc):
-    data_gen = cg.generate_data(rhythm_context_size=rc,
-                            melody_context_size=mc,
-                            with_metaData=True)
-    
-    while True:
-        for x, y in data_gen:
-            yield (x[:-2], y)
-        
-        data_gen = cg.generate_data(rhythm_context_size=rc,
-                            melody_context_size=mc,
-                            with_metaData=True)
-
-#%%
 if __name__ == "__main__":
- #%%   
-    top_dir = "Trainings"
-    save_dir = "test"
+   
+    top_dir = "Trainings"    
+#    save_dir = asctime().split()
+#    save_dir = "_".join([*save_dir[0:3], *save_dir[3].split(":")[:2]])
+    save_dir = "first_with_lead"
 
-    num_epochs = 10
-    j = 1   # checkpoint frequency
+    num_epochs = 200
+    j = 2   # checkpoint frequency
     
     
     # META
@@ -49,7 +37,7 @@ if __name__ == "__main__":
 
     # CHANGE
     music_dir = "../../Data/music21/"
-    cg = CombinedGenerator(music_dir, save_conversion_params="Trainings/test",
+    cg = CombinedGenerator(music_dir, save_conversion_params="/".join([top_dir, save_dir]),
                            to_list=0, meta_prep_f=meta_embedder.predict)
     cg.get_num_pieces()
     
@@ -57,12 +45,9 @@ if __name__ == "__main__":
     mc_size = 4
     data_iter = cg.generate_forever(rhythm_context_size=rc_size, 
                                     melody_context_size=mc_size, 
-                                    with_metaData=True)
-    
-#    data_iter = without_lead(cg, rc_size, mc_size)
+                                    with_metaData=True)    
     print("\nData generator set up...\n")
     
- #%%   
     # PARAMS
     
     #rhythm params
@@ -86,10 +71,8 @@ if __name__ == "__main__":
     melody_dec_lstm_size = 32
     
     meta_data_len = 10
-    
-#%%    
+     
     # INDIVIDUAL NETS
-    
     bar_embedder = BarEmbedding(V=V_rhythm, beat_embed_size=beat_embed_size, 
                                 embed_lstm_size=embed_lstm_size, 
                                 out_size=out_size)
@@ -100,7 +83,6 @@ if __name__ == "__main__":
                                dec_lstm_size=rhythm_dec_lstm_size, V=V_rhythm, 
                                dec_use_meta=True, compile_now=True)
     
-#%%
     # ATTENTION: conv_win_size must not be greater than context size!
     melody_encoder = MelodyEncoder(m=m, conv_f=conv_f, conv_win_size=min(mc_size, conv_win_size), 
                                    enc_lstm_size=melody_enc_lstm_size)
@@ -111,24 +93,23 @@ if __name__ == "__main__":
 
     
     print("Individual networks set up...\n")
-    
-#%%    
-    #
+       
+    # COMBINED NETWORK
     comb_net = CombinedNetwork(context_size, m, meta_embed_size, 
                                bar_embedder, rhythm_net, melody_net, meta_predictor,
                                generation=False, compile_now=True)
     
     print("Combined network set up...\n")
-#%%    
-    #
     
-    if not save_dir:
-        raise FileNotFoundError("DIRECTORY TO SAVE IS NONE, DON'T KNOW WHERE TO SAVE!" + 
+    # TRAINING
+    
+    if not save_dir or not os.path.isdir("/".join([top_dir, save_dir])):
+        raise FileNotFoundError("DIRECTORY TO SAVE IS NONE OR DOESN'T EXISTS, DON'T KNOW WHERE TO SAVE!" + 
                                 "\nHINT: USE DIRECTORY WHERE META DATA IS SAVED")
     log_dir = "logs"
     weight_dir = "weights"
-    
-    cur_date_time = asctime().replace(" ", "_").replace(":", "-")
+
+
     tb = TensorBoard(log_dir="/".join([top_dir, save_dir, log_dir]))
     
     if os.path.isdir("/".join([top_dir, save_dir, weight_dir])):
@@ -137,7 +118,7 @@ if __name__ == "__main__":
     os.makedirs("/".join([top_dir, save_dir, weight_dir]))
     
     
-    #
+    # TRAINING LOOP
     for cur_iteration in range(int(num_epochs/j)):
     
         print("\nITERATION ", cur_iteration)
